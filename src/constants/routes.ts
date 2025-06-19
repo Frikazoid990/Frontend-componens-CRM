be2d43866cc4ab579acc402ae74701421b9aa056
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-06-19 23:03:57 +0300

    add api routs #1

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,7 +2,19 @@
 
 
 	export const carApiRoutes = {
-
+		allCar :'/car/getallcars',
 	}
 
-	
+	export const authApiRoutes = {
+		signIn: '/auth/signIn',
+		registration: '/auth/registration',
+	}
+
+	export const dealApiRoutes = {
+		addDeal: '/deal/add-deal',
+
+		updateDealWithEmployee: (dealId: string) =>  `/deal/update-deal-with-employee/${dealId}`,
+
+		updateDealWithStatus: (dealId: string) => `/deal/update-deal-with-status/${dealId}`,
+
+	}
